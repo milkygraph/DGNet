563c2eefb3444a0970f8ecdaf86d02fc4b2b0346
MHD kamal rushdi <rushdi21@itu.edu.tr>
2024-12-24 03:25:34 +0300

    correcting export_objs.py

--- labeling_automators/export_objs.py
+++ labeling_automators/export_objs.py
@@ -1,4 +1,3 @@
-"""for script classes obj export by hand"""
 import bpy
 
 def export_mesh(obj, obj_path):
@@ -9,13 +8,34 @@ def export_mesh(obj, obj_path):
         export_vertex_groups=True
     )
 
+def read_number(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            number = int(file.read().strip())
+            return number
+    except FileNotFoundError:
+        return 1  # Return 1 if the file does not exist
+
+def write_number(file_path, number):
+    with open(file_path, 'w') as file:
+        file.write(str(number))
+
+# File path where the number is stored
+number_file_path = '../labeled/sample_number.txt'
+
 # Get the active object
 active_obj = bpy.context.object
 
-# Set the file paths for OBJ and PLY
-obj_file_path = "../labeled/script_obj_with_classes.obj"
+# Read the current number from the file
+current_number = read_number(number_file_path)
+
+# Set the file path for OBJ based on the current number
+obj_file_path = f"../labeled/objs/raw/raw_{current_number}.obj"
 
 # Call the export function
 export_mesh(active_obj, obj_file_path)
+
+# Increment the number and write it back to the file
+write_number(number_file_path, current_number + 1)
 
 print("Export completed.")
