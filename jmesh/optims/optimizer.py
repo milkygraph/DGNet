ea477d8e9e823cf485f98217eb1b4a4081a9e08f
Kenan Bandaliyev <kenan.bandaliyev@gmail.com>
2024-12-31 13:53:58 +0300

    update optimizer code

--- jmesh/optims/optimizer.py
+++ jmesh/optims/optimizer.py
@@ -1,43 +1,58 @@
-from jmesh.utils.registry import OPTIMS 
+from jmesh.utils.registry import OPTIMS
 
-from jittor import optim 
+from jittor import optim
 import jittor as jt
+
 
 class Optimizer(object):
     def parameters(self):
         data = {}
-        for k,d in self.__dict__.items():
+        for k, d in self.__dict__.items():
             if k == "param_groups":
                 continue
-            data[k]=d 
+            data[k] = d
         return data
 
-    def load_parameters(self,data):
-        if isinstance(data,dict):
-            for k,d in data.items():
+    def load_parameters(self, data):
+        if isinstance(data, dict):
+            for k, d in data.items():
                 if k in self.__dict__:
-                    self.__dict__[k]=d 
+                    self.__dict__[k] = d
+
     def cur_lr(self):
-        return self.param_groups[0].get("lr",self.lr)
-    
+        return self.param_groups[0].get("lr", self.lr)
+
+
 @OPTIMS.register_module()
-class SGD(optim.SGD,Optimizer):
-    def __init__(self,params, lr, momentum=0, weight_decay=0, dampening=0, nesterov=False,grad_clip=None):
-        super(SGD,self).__init__(params, lr, momentum, weight_decay, dampening, nesterov)
+class SGD(optim.SGD, Optimizer):
+    def __init__(
+        self,
+        params,
+        lr,
+        momentum=0,
+        weight_decay=0,
+        dampening=0,
+        nesterov=False,
+        grad_clip=None,
+    ):
+        super(SGD, self).__init__(
+            params, lr, momentum, weight_decay, dampening, nesterov
+        )
         self.grad_clip = grad_clip
 
-    def pre_step(self, loss):
-        super(SGD,self).pre_step(loss)
+    def pre_step(self, loss, retain_graph=False):
+        super(SGD, self).pre_step(loss)
         if self.grad_clip is not None:
             self.clip_grad_norm(**self.grad_clip)
-    
+
+
 @OPTIMS.register_module()
-class GradMutilpySGD(optim.SGD,Optimizer):
-    def __init__(self,grad_clip=None, **kwargs):
-        super(GradMutilpySGD,self).__init__(**kwargs)
+class GradMutilpySGD(optim.SGD, Optimizer):
+    def __init__(self, grad_clip=None, **kwargs):
+        super(GradMutilpySGD, self).__init__(**kwargs)
         self.grad_clip = grad_clip
 
-    def step(self, loss):
+    def step(self, loss, retain_graph=False):
         if loss is not None:
             self.pre_step(loss)
         if self.grad_clip is not None:
@@ -53,7 +68,8 @@ class GradMutilpySGD(optim.SGD,Optimizer):
             m = pg.get("grad_mutilpy", 1)
             # optimize main body
             for p, g, v in zip(pg["params"], pg["grads"], pg["values"]):
-                if p.is_stop_grad(): continue
+                if p.is_stop_grad():
+                    continue
                 dp = p * weight_decay + g * m
                 v.update(momentum * v + dp * (1 - dampening))
                 if nesterov:
@@ -64,24 +80,24 @@ class GradMutilpySGD(optim.SGD,Optimizer):
 
 
 @OPTIMS.register_module()
-class Adam(optim.Adam,Optimizer):
-    def __init__(self,grad_clip=None,**kwargs):
-        super().__init__(**kwargs) 
+class Adam(optim.Adam, Optimizer):
+    def __init__(self, grad_clip=None, **kwargs):
+        super().__init__(**kwargs)
         self.grad_clip = grad_clip
 
-    def pre_step(self, loss):
-        super(Adam,self).pre_step(loss)
+    def pre_step(self, loss, retain_graph=False):
+        super(Adam, self).pre_step(loss)
         if self.grad_clip is not None:
             self.clip_grad_norm(**self.grad_clip)
-    
+
 
 @OPTIMS.register_module()
-class AdamW(optim.AdamW,Optimizer):
-    def __init__(self,grad_clip=None,**kwargs):
-        super().__init__(**kwargs) 
+class AdamW(optim.AdamW, Optimizer):
+    def __init__(self, grad_clip=None, **kwargs):
+        super().__init__(**kwargs)
         self.grad_clip = grad_clip
 
-    def pre_step(self, loss):
-        super(AdamW,self).pre_step(loss)
+    def pre_step(self, loss, retain_graph=False):
+        super(AdamW, self).pre_step(loss)
         if self.grad_clip is not None:
             self.clip_grad_norm(**self.grad_clip)
