333ccb67aed68956687bb8695f90240838d23047
mhd-kamal-rushdi <rushdi21@itu.edu.tr>
2025-03-06 14:41:04 +0300

    changed compile.sh python interpeter version

--- jmesh/utils/compile.sh
+++ jmesh/utils/compile.sh
@@ -3,4 +3,4 @@
 # sudo cp -r /usr/include/eigen3/Eigen /usr/local/include
 # python3 -m pip install pybind11 
 
-g++ -O3 -Wall -shared -std=c++11 -fPIC -I/usr/include/python3.8 `python3 -m pybind11 --includes` jmesh_simplify.cpp -o jmesh_simplify`python3-config --extension-suffix`
+g++ -O3 -Wall -shared -std=c++11 -fPIC -I/usr/include/python3.10 `python3 -m pybind11 --includes` jmesh_simplify.cpp -o jmesh_simplify`python3-config --extension-suffix`
